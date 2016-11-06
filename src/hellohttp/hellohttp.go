bd6308aed326b99d106cd0ba8bb021a8066c5e27
Dave Cheney <dave@cheney.net>
2016-11-06 12:04:46 +1100

    added string types

--- src/hellohttp/hellohttp.go
+++ src/hellohttp/hellohttp.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+const templ = `<html><head><title>Hello</title></head><body>
+Hello {{ .RemoteAddr }}
+You sent me a {{ .Method }} request for {{ .URL }}
+</body></html>`
+
 func HelloServer(w http.ResponseWriter, req *http.Request) {
 	log.Println(req.URL)
-	fmt.Fprintf(w, "Hello, 世界!\nURL = %s\n", req.URL)
+	t := template.Must(template.New("html").Parse(templ))
+	t.Execute(w, req)
 }
 
 func main() {
---
+++ src/strings/strings1/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// START OMIT
+	var salutation = "Hello"
+	var name = "David"
+
+	var greeting = salutation + " " + name // HL
+	println(greeting)
+	// END OMIT
+}
