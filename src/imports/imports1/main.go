cba0578a65590b10d9465a864b9b342fdc328fbd
Dave Cheney <dave@cheney.net>
2016-11-07 21:57:59 +1100

    added import section

---
+++ src/imports/imports1/main.go
@@ -0,0 +1,10 @@
+package main
+
+var now = time.Now() // HL
+
+import "fmt"
+import "time"
+
+func main() {
+	fmt.Println(now)
+}
---
+++ src/imports/imports2/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+	var now = time.now() // HL
+	fmt.println(now)     // HL
+}
